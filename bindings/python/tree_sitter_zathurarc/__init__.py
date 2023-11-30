5c10ea9a25b347d062bc2ac19e8fbd00f2a2f3e8
Wu Zhenyu <wuzhenyu@ustc.edu>
2023-11-30 23:52:51 +0800

    :sparkles: Add python binding

---
+++ bindings/python/tree_sitter_zathurarc/__init__.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+from tree_sitter import Language, Parser
+
+__version__ = "0.0.1"
+__version_tuple__ = (0, 0, 1, "", "")
+
+language = Language(
+    str(next((Path(__file__).parent / "data" / "lib").glob("zathurarc.*"))),
+    "zathurarc",
+)
+parser = Parser()
+parser.set_language(language)
