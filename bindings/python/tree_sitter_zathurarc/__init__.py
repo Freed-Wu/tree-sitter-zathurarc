81b87a0188ac8cc8f50fd105bbf14fdebc3dcede
Wu Zhenyu <wuzhenyu@ustc.edu>
2023-12-02 01:09:28 +0800

    :heavy_plus_sign: Add cmake

--- bindings/python/tree_sitter_zathurarc/__init__.py
+++ bindings/python/tree_sitter_zathurarc/__init__.py
@@ -9,7 +9,7 @@ __version__ = "0.0.1"
 __version_tuple__ = (0, 0, 1, "", "")
 
 language = Language(
-    str(next((Path(__file__).parent / "data" / "lib").glob("zathurarc.*"))),
+    str(next((Path(__file__).parent / "data" / "lib").glob("libparser.*"))),
     "zathurarc",
 )
 parser = Parser()
