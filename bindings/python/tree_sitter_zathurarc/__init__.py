8ebafc515ca62e8f9994135de2eaa6c01dbca244
Wu Zhenyu <wuzhenyu@ustc.edu>
2023-12-05 22:32:08 +0800

    :bug: Fix wrong version

--- bindings/python/tree_sitter_zathurarc/__init__.py
+++ bindings/python/tree_sitter_zathurarc/__init__.py
@@ -5,8 +5,11 @@ from pathlib import Path
 
 from tree_sitter import Language, Parser
 
-__version__ = "0.0.1"
-__version_tuple__ = (0, 0, 1, "", "")
+try:
+    from ._version import __version__, __version_tuple__  # type: ignore
+except ImportError:  # for setuptools-generate
+    __version__ = "rolling"
+    __version_tuple__ = (0, 0, 0, __version__, "")
 
 language = Language(
     str(next((Path(__file__).parent / "data" / "lib").glob("libparser.*"))),
