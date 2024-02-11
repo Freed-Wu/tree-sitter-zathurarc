a62d47b82475e5b03c4b1a2b12b46a233f428478
Wu Zhenyu <wuzhenyu@ustc.edu>
2024-02-11 11:15:04 +0800

    :hammer: Refactor CMakeLists.txt

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,6 @@
 cd "$(dirname "$(readlink -f "$0")")/.." || exit 1
 
 tree-sitter generate
-cmake -Bbuild
+cmake -Bbuild -DCMAKE_INSTALL_FULL_LIBDIR="${1:-$HOME/.local/share/nvim/repos/github.com/nvim-treesitter/nvim-treesitter}"
 cmake --build build
-cp build/libparser.so "${1:-$HOME/.local/share/nvim/repos/github.com/nvim-treesitter/nvim-treesitter/parser}/zathurarc.so"
+cmake --install build
