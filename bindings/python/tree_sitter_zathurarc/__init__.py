3e914b550690f4e8210aeab0aee98420e5bdd768
Wu Zhenyu <wuzhenyu@ustc.edu>
2024-02-11 11:15:04 +0800

    :hammer: Refactor CMakeLists.txt

--- bindings/python/tree_sitter_zathurarc/__init__.py
+++ bindings/python/tree_sitter_zathurarc/__init__.py
@@ -2,9 +2,17 @@ r"""Tree sitter
 ===============
 """
 
-from pathlib import Path
+import os
+import sysconfig
 
 from tree_sitter import Language, Parser
+
+from . import __name__ as NAME
+
+NAME = NAME.split("_")[-1]
+DLL = sysconfig.get_config_var("SHLIB_SUFFIX")
+if DLL is None:
+    DLL = ".dll"
 
 try:
     from ._version import __version__, __version_tuple__  # type: ignore
@@ -12,9 +20,23 @@ except ImportError:  # for setuptools-generate
     __version__ = "rolling"
     __version_tuple__ = (0, 0, 0, __version__, "")
 
-language = Language(
-    str(next((Path(__file__).parent / "data" / "lib").glob("libparser.*"))),
-    "zathurarc",
-)
+paths = [
+    os.path.join(
+        os.path.join(
+            os.path.join(sysconfig.get_path("data", scheme), "lib"), "parser"
+        ),
+        NAME + DLL,
+    )
+    for scheme in [
+        sysconfig.get_preferred_scheme("user"),
+        sysconfig.get_default_scheme(),
+    ]
+]
+for path in paths:
+    if os.path.isfile(path):
+        break
+else:
+    raise FileNotFoundError(paths)
+language = Language(path, NAME)
 parser = Parser()
 parser.set_language(language)
